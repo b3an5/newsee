0b3008fb86fae8bd30df8a1491f4828ac3e745dc
B3an5 <beantheo@gmail.com>
2019-05-31 21:44:25 -0600

    actions

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const saveTopStories = article => ({
+  type: "SAVE_TOP_STORIES",
+  article
+});
+
+export const saveSearchStories = article => ({
+  type: "SAVE_SEARCH_STORIES",
+  article
+});
+
+export const saveCurrentStory = article => ({
+  type: "SAVE_CURRENT_STORY",
+  article
+});
