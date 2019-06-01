6c1e1e09aa4c8ceb19fe15aa1853584fe86007c3
B3an5 <beantheo@gmail.com>
2019-05-31 21:45:17 -0600

    top headline api call

---
+++ src/utilities/topHeadlinesCall.js
@@ -0,0 +1,10 @@
+export const topHeadlineData = async () => {
+  var url =
+    "https://newsapi.org/v2/top-headlines?" +
+    "country=us&" +
+    "apiKey=206492147a2649c9b1ac498dcd498198";
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result.articles);
+  return result.articles;
+};
