bb3257887587eab3c4064c10a5a543dd7264b2bd
B3an5 <beantheo@gmail.com>
2019-05-31 21:45:00 -0600

    search reducer

---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,8 @@
+export const SearchReducer = (state = [], action) => {
+  switch (action.type) {
+    case "SAVE_SEARCH_STORIES":
+      return action.article;
+    default:
+      return state;
+  }
+};
