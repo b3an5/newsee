1066054718206cd8f14466b3e8afa04d091e462f
B3an5 <beantheo@gmail.com>
2019-06-01 21:45:36 -0600

    display full article with local storage

--- src/Containers/FullStory/FullStory.js
+++ src/Containers/FullStory/FullStory.js
@@ -1,22 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
-function FullStory(props) {
-  const {
-    title,
-    urlToImage,
-    content,
-    author,
-    description
-  } = props.currentStory;
-  return (
-    <main>
-      <img src={urlToImage} alt={title} />
-      <h1>{title}</h1>
-      <h2>{author}</h2>
-      <p>{content}</p>
-    </main>
-  );
+class FullStory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      article: {}
+    };
+  }
+
+  componentDidMount = () => {
+    console.log(JSON.parse(localStorage.article).publishedAt);
+    console.log(this.props.match.params.id);
+    if (
+      this.props.match.params.id.includes(
+        JSON.parse(localStorage.article).publishedAt
+      )
+    ) {
+      this.setState({ article: JSON.parse(localStorage.article) });
+    } else {
+      this.grabArticleContent(this.props.currentStory.url);
+    }
+  };
+
+  grabArticleContent = async articleUrl => {
+    const {
+      title,
+      urlToImage,
+      author,
+      url,
+      publishedAt
+    } = this.props.currentStory;
+    const fetchUrl = `https://api.diffbot.com/v3/article?token=27b09f6cb2a8e2ba60bf2717c2e9326f&url=${articleUrl}`;
+    const response = await fetch(fetchUrl);
+    const result = await response.json();
+    if (!result.objects[0].text) {
+      return alert("sorry");
+    }
+    const currentArticle = {
+      title,
+      url,
+      author,
+      content: result.objects[0].text,
+      urlToImage,
+      publishedAt
+    };
+    localStorage.setItem("article", JSON.stringify(currentArticle));
+    this.setState({ article: currentArticle });
+  };
+
+  render() {
+    const { title, urlToImage, content, author, url } = this.state.article;
+
+    if (this.state.articleHtml !== null) {
+      return (
+        <main>
+          <img src={urlToImage} alt={title} />
+          <h1>{title}</h1>
+          <h2>{author}</h2>
+          <p>{content}</p>
+          <a href={url}>read the original article here</a>
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          <img src={urlToImage} alt={title} />
+          <h1>{title}</h1>
+          <h2>{author}</h2>
+          <p>...loading</p>
+          <a href={url}>read the original article here</a>
+        </main>
+      );
+    }
+  }
 }
 
 export const mapStateToProps = state => ({
