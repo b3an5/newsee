69a2c635ac67af95f592e3c6c0ee6ebe6e535e53
B3an5 <beantheo@gmail.com>
2019-06-03 14:05:16 -0600

    css changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import ArticleContainer from "../../Containers/ArticleContainer/ArticleContainer";
+import "./Home.scss";
 
 export default function Home() {
   return (
     <main>
       <header>
         <h1 className="title">Newsee</h1>
+        <h4 className="description">Go Ahead use AdBlock, we dont care.</h4>
       </header>
       <ArticleContainer />
     </main>
