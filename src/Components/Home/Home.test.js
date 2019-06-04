7768e7b61fbae39746fca71013596837a660a541
B3an5 <beantheo@gmail.com>
2019-06-04 16:42:27 -0600

    home test

---
+++ src/Components/Home/Home.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Home from "./Home";
+
+describe("Home", () => {
+  let wrapper;
+  it("should match snapshot", () => {
+    wrapper = shallow(<Home />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
